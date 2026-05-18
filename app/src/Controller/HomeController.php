ce919f1d2e7f16e49064e185e21d74a2adf49af6
Andrea <chiappi.andrea@gmail.com>
2026-05-18 07:30:04 +0200

    v 0.1.1 - rimosso metodo getDokerStats

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -15,36 +15,4 @@ final class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
-
-    public function getDockerStats(): array
-    {
-        $chandler = curl_init();
-        curl_setopt($chandler, CURLOPT_UNIX_SOCKET_PATH, "/var/run/docker.sock");
-        curl_setopt($chandler, CURLOPT_URL, "http://localhost/containers/json?all=1");
-        curl_setopt($chandler, CURLOPT_RETURNTRANSFER, true);
-
-        $jsonResponse = curl_exec($chandler);
-
-        if ($jsonResponse === false) {
-            $error = curl_error($chandler);
-            curl_close($chandler);
-            return ['error' => 'Errore connessione Socket: ' . $error];
-        }
-
-        curl_close($chandler);
-
-        return json_decode($jsonResponse, true);
-    }
-
-    #[Route('/test', name: 'app_test')]
-    public function test(): Response
-    {
-        $test = $this->getDockerStats();
-
-        dd($test);
-
-        return $this->render('/test', [
-            'containers' => $test,
-        ]);
-    }
 }
