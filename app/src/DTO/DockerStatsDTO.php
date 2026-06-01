690b492bf0cfb5a3c3855c1c30a89953aa59506a
Andrea <chiappi.andrea@gmail.com>
2026-06-01 07:35:53 +0200

    v 0.10.13 - aggiunta attributo serializer + metodo pulizia nome

--- app/src/DTO/DockerStatsDTO.php
+++ app/src/DTO/DockerStatsDTO.php
@@ -2,14 +2,30 @@
 
 namespace App\DTO;
 
+use Symfony\Component\Serializer\Attribute\SerializedName;
+
 class DockerStatsDTO
 {
     public function __construct(
+        #[SerializedName('Id')]
         public readonly string $id,
-        public readonly string $name,
-        public readonly string $state,
+
+        #[SerializedName('Names')]
+        public readonly array $names,
+
+        #[SerializedName('Status')]
         public readonly string $status,
+
+        #[SerializedName('State')]
+        public readonly string $state,
+
+        #[SerializedName('Health')]
         public readonly string $health
     )
     {}
+
+    public function getCleanNames()
+    {
+        return isset($this->names[0]) ? ltrim($this->names[0], '/') : 'N/A';
+    }
 }
