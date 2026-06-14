ef4a1dfd9a54e82bcbaa4cbfb8b3d189ad8d97b2
Andrea <chiappi.andrea@gmail.com>
2026-06-14 19:45:26 +0200

    v 0.11.27 - aggiunti metodi stampa; visualizzazione raw data

--- app/src/Controller/TestController.php
+++ app/src/Controller/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\DockerService;
+use JetBrains\PhpStorm\NoReturn;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,15 +11,34 @@ use Symfony\Component\Routing\Attribute\Route;
 final class TestController extends AbstractController
 {
 
+    #[NoReturn]
     #[Route('/test', name: 'app_test')]
     public function test(DockerService $dockerService): Response
     {
+        $raw = $dockerService->getDockerStatsRaw();
+
+        $rawStampa = json_decode($raw, true);
+
         $test = $dockerService->getDockerStatsDTO();
 
-        dd($test);
+        $risposta = $this->stampaTest($test);
 
-        return $this->render('/test', [
-            'containers' => $test,
-        ]);
+        dd($rawStampa, $test, $risposta);
+    }
+
+    private function stampaTest($test): array
+    {
+        $rows = [];
+        foreach ($test as $statsDTO) {
+            $rows[] = [
+                $statsDTO->id,
+                $statsDTO->getCleanNames(),
+                $statsDTO->getCleanStatus(),
+                $statsDTO->state,
+                $statsDTO->getHealthStatus()
+            ];
+        }
+
+        return $rows;
     }
 }
