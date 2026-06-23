ef4745b86ace05f8b3f3985afbb6955eec937917
Andrea <chiappi.andrea@gmail.com>
2026-06-23 22:37:13 +0200

    v 0.20.41 - aggiunto metodo get

--- app/src/Controller/DockerStatsController.php
+++ app/src/Controller/DockerStatsController.php
@@ -2,16 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\ContainerStats;
+use App\Service\DockerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
-#[Route('/docker-stats', name: 'app_docker_stats')]
+#[Route('/docker-stats', name: 'docker_stats')]
 class DockerStatsController extends AbstractController
 {
     public function __construct(
-
+        private readonly SerializerInterface $serializer,
     )
     {
+    }
 
+    #[Route('', name: '', methods: ['GET'])]
+    public function getContainerStats(DockerService $dockerService) : Response
+    {
+        $stats = $dockerService->getStatsDB();
+
+        try {
+            return new Response($this->serializer->serialize($stats, 'json', [ContainerStats::class]), Response::HTTP_OK);
+        } catch (ExceptionInterface $e) {
+            return new Response($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
